0212377802a123056f148be18b49fdc69d0c1e1a
oyyl <oyai32@126.com>
2023-03-05 18:13:54 +0800

    feat: 引入 antd 完成 layout 和 login 的样式

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,7 +1,81 @@
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  LogoutOutlined
+} from '@ant-design/icons';
+import { Layout, Menu, Popconfirm } from 'antd';
+import React, { useState } from 'react';
 import './index.scss';
+const { Header, Sider, Content } = Layout;
 
-function Layout () {
-  return <div className="Layout">Layout</div>;
+function logout () {
 }
 
-export default Layout;
+function App () {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout className="layout">
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="header"
+          style={{
+            background: '#fff',
+          }}
+        >
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+          <div class="header-info">
+            <span className="user-name"><UserOutlined />&nbsp;&nbsp;xxxx</span>
+            <span className="user-logout">
+              <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消" onConfirm={logout}>
+                <LogoutOutlined />&nbsp;&nbsp;退出
+              </Popconfirm>
+            </span>
+          </div>
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: '#fff',
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+export default App;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,59 @@
 
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
 import './index.scss';
 
+const onFinish = (values) => {
+  console.log('Success:', values);
+};
+
 function Login () {
-  return <div className="Login">Login</div>;
+  return (
+    <div className="login">
+      <div className="login-form">
+        <div className="login-title">登&nbsp;&nbsp;&nbsp;&nbsp;录</div>
+        <Form
+          name="basic"
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: '请输入账号!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              立即登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
